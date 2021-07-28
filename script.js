81bc643acee8ff2d601c00fb04cb88f5c25cf6ed
Ali Cetindag <78816804+Alice-63@users.noreply.github.com>
2021-07-28 23:21:59 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,38 @@
+
+setTimeout(function(){$(document).ready(function(){
+    $("#message").click(function(){
+        $("#message").animate({
+            left: "25vw",
+            top:"20vh",
+            width:"700px",
+            height:"430px",
+            fontSize:"36px",
+           
+        },2000,function(){
+            $("#icon").fadeIn(500)
+            $("#iconMinus").fadeIn(500)
+        })
+    })
+
+    $("#icon").click(function(){
+        $("#message").slideUp(1000)
+    })
+    
+    
+})
+$("#iconMinus").click(function(){
+    $("#message").animate({
+        left: "68vw",
+        top:"73vh",
+        width:"400px",
+        height:"130px",
+        fontSize:"20px",
+        padding:"25px"
+       
+    },1000,function(){
+        $("#message").stop(true,true)
+        $("#iconMinus").hide()
+        $("#icon").hide()
+
+    })
+})},3000)
